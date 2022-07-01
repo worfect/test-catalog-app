db7594b0c07deee1730db86925ac4a32e0582981
worfect <senchenkov.93@gmail.com>
2022-07-01 15:02:09 +0800

    upd draft routes, view, contr

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Material;
+use App\Models\Tag;
+
 class MaterialController extends Controller
 {
-    public function show(){
-        return view('list.material')->with(['name' => 'material']);
+    public function show(Material $material, Tag $tag, $id = null)
+    {
+        if($id){
+            return view('view.material')->with(['material' => $material->with($material->relations)->find($id),
+                                                     'tags' => $tag->all()]);
+        }
+        return view('list.material')->with(['items' => $material->all()]);
+    }
+
+    public function edit(Material $material, $id = null)
+    {
+        if($id){
+            return view('edit.material')->with(['item' => $material->with($material->relations)->find($id)]);
+        }
+        return view('edit.material');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,12 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/material');
 
 Route::controller(MaterialController::class)->name('material.')->group(function () {
+    Route::get('/material/edit/{id?}', 'edit')->name('edit');
     Route::get('/material/{id?}', 'show')->name('show');
 });
 
 Route::controller(TagController::class)->name('tag.')->group(function () {
+    Route::get('/tag/edit/{id?}', 'edit')->name('edit');
     Route::get('/tag/{id?}', 'show')->name('show');
 });
 
