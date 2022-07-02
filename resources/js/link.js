778f396f366320cb8698ed789255b3a83f6bda04
worfect <senchenkov.93@gmail.com>
2022-07-02 22:00:31 +0800

    hot fix js

--- resources/js/link.js
+++ resources/js/link.js
@@ -5,31 +5,32 @@ window.$ = window.jQuery = $;
 const host = 'http://localhost:81'
 
 const linkModal = document.getElementById('linkModalToggle');
+if(linkModal){
+    linkModal.addEventListener('show.bs.modal', function (event) {
 
-linkModal.addEventListener('show.bs.modal', function (event) {
+        let button = event.relatedTarget;
 
-    let button = event.relatedTarget;
+        let id = button.getAttribute('data-bs-id');
+        let title = button.getAttribute('data-bs-title');
+        let url = button.getAttribute('data-bs-url');
+        let material = button.getAttribute('data-bs-material');
 
-    let id = button.getAttribute('data-bs-id');
-    let title = button.getAttribute('data-bs-title');
-    let url = button.getAttribute('data-bs-url');
-    let material = button.getAttribute('data-bs-material');
+        let idInput = linkModal.querySelector('.modal-body .linkId');
+        let titleInput = linkModal.querySelector('.modal-body .linkTitle');
+        let urlInput = linkModal.querySelector('.modal-body .linkUrl');
 
-    let idInput = linkModal.querySelector('.modal-body .linkId');
-    let titleInput = linkModal.querySelector('.modal-body .linkTitle');
-    let urlInput = linkModal.querySelector('.modal-body .linkUrl');
+        titleInput.value = title;
+        urlInput.value = url;
 
-    titleInput.value = title;
-    urlInput.value = url;
+        let form = linkModal.querySelector('.modal-content form');
+        if(id){
+            idInput.value = id;
+            form.action = host + '/link/' + id + '/edit';
+        }
+        else{
+            idInput.value = material;
+            form.action = host + '/link/create';
+        }
 
-    let form = linkModal.querySelector('.modal-content form');
-    if(id){
-        idInput.value = id;
-        form.action = host + '/link/' + id + '/edit';
-    }
-    else{
-        idInput.value = material;
-        form.action = host + '/link/create';
-    }
-
-})
+    })
+}
