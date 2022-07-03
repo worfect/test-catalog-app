871ee87c5969bc24c6562b8cc1fc49a86a0f7ae2
worfect <senchenkov.93@gmail.com>
2022-07-03 12:22:39 +0800

    added tag list stuff

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -5,16 +5,29 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Requests\BindTagRequest;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class TagController extends Controller
 {
-    public function index(){
-        return view('list.tag')->with(['name' => 'tag']);
+    public function index(Tag $tag)
+    {
+        return view('list.tag')->with(['tags' => $tag->all()]);
     }
 
-    public function bind(BindTagRequest $request){
+    public function edit(Tag $tag, $id)
+    {
+        return view('edit.tag')->with(['tag' => $tag->find($id)]);
+    }
+
+    public function create()
+    {
+        return view('create.tag');
+    }
+
+    public function bind(BindTagRequest $request)
+    {
         DB::table('material_tag')->insert([
             'material_id' => $request->get('materialId'),
             'tag_id' => $request->get('tagId')
@@ -23,12 +36,20 @@ class TagController extends Controller
         return redirect()->route('material.show', $request->get('materialId'));
     }
 
-    public function unbind(Request $request){
+    public function unbind(Request $request)
+    {
         DB::table('material_tag')->where([
             'material_id' => $request->get('materialId'),
             'tag_id' => $request->get('tagId')
         ])->delete();
 
         return redirect()->route('material.show', ['id' => $request->get('materialId')]);
+    }
+
+    public function remove(Tag $tag, $id)
+    {
+        $tag->find($id)->delete();
+
+        return redirect()->back();
     }
 }
